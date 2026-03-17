15c7011cca74c21f3f89903f4d1d40ded5fa4128
bkocii <burimkoci@hotmail.com>
2026-03-17 18:26:39 +0100

    Add login flow and basic page structure

---
+++ orders/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("waiter/", views.waiter_order_page, name="waiter_order_page"),
+    path("live/", views.live_orders_page, name="live_orders_page"),
+]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def home(request):
+    return render(request, "orders/home.html")
+
+
+@login_required
+def waiter_order_page(request):
+    return render(request, "orders/waiter_order_page.html")
+
+
+@login_required
+def live_orders_page(request):
+    return render(request, "orders/live_orders_page.html")
