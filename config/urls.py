256feed817122617b390c7ca9b8cf733fa52c2d5
bkocii <burimkoci@hotmail.com>
2026-03-19 16:06:36 +0100

    Fix project static files setup and polish shift summary layout

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,3 +10,5 @@ urlpatterns = [
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", include("orders.urls")),
 ]
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
